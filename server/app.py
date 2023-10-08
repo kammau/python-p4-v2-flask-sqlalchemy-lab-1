01b7c3606a8a9d4767b7065fbe8a2b24c041d280
Kamryn Smith <programersign@gmail.com>
2023-10-08 10:21:15 -0700

    assignment complete

--- server/app.py
+++ server/app.py
@@ -21,6 +21,30 @@ def index():
     return make_response(body, 200)
 
 # Add views here
+@app.route("/earthquakes/<int:id>")
+def earthquake_by_id(id):
+    earthquake = Earthquake.query.filter_by(id=id).first()
+
+    if earthquake:
+        response = earthquake.to_dict()
+        status = 200
+    else:
+        response = {"message": f"Earthquake {id} not found."}
+        status = 404
+    
+    return make_response(response, status)
+
+@app.route("/earthquakes/magnitude/<float:magnitude>")
+def earthquake_by_magnitude(magnitude):
+    earthquakes = [earthquake.to_dict() for earthquake in Earthquake.query.filter(Earthquake.magnitude >= magnitude).all()]
+
+    response_body = {
+        "count" : len(earthquakes),
+        "quakes": earthquakes,
+    }
+
+    return make_response(response_body, 200)
+
 
 
 if __name__ == '__main__':
